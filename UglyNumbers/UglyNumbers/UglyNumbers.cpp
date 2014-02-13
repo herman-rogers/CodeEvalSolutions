bc7e0eedb15055bd30aa9d46c36f218e0b35617d
Admin <Admin@WXPPx86BE-5809.(none)>
2014-02-13 14:02:00 -0700

    Updates to ugly numbers code eval problem

--- UglyNumbers/UglyNumbers/UglyNumbers.cpp
+++ UglyNumbers/UglyNumbers/UglyNumbers.cpp
@@ -10,32 +10,39 @@ typedef list<float> NumberList;
 NumberList listOfSplitNumbers;
 NumberList listOfUglyNumbers;
 int uglyPrimes[4] = {2,3,5,7};
-int selectedNumber;
+//int selectedNumber;
 
-bool IsNumberUgly( ){
+bool IsNumberUgly( int selectedNumber ){
 	for( int i = 0; i < 4; i++ ){
 		//add in a check if your the selectednumber IS
 		// itself a uglyprime
 		if ( selectedNumber / uglyPrimes[i] == 0 ){
-			return false;
+			return true;
 		}
 	}
-	return true;
+	return false;
 }
 
 void FindCombinations( ){
-	float lengthOfList = ( ( float )listOfSplitNumbers.size( ) - 1 );
-	float lengthOfCombinations = pow( 3, lengthOfList );
+	//float lengthOfList = ( ( float )listOfSplitNumbers.size( ) - 1 );
+	//float lengthOfCombinations = pow( 3, lengthOfList );
+	int tempNumber = 0;
 	for( NumberList::const_iterator cIter = listOfSplitNumbers.begin( );
-		 cIter != listOfSplitNumbers.end( ); cIter++ ){
-		cout << ( *cIter );
+		cIter != listOfSplitNumbers.end( ); cIter++ ){
+			cout << ( *cIter );
+			tempNumber = tempNumber + *cIter;
+	}
+	if ( IsNumberUgly( tempNumber ) ){
+		listOfUglyNumbers.push_front( tempNumber );
 	}
 }
 
-void SplitNumber( ){
-	float parseOutNumber  = 0.0f;
-	for( int i = 1; i <= selectedNumber; i*=10 ){
-		parseOutNumber = ( selectedNumber / i ) % 10;
+void SplitString( string selectedNumber ){
+	float parseOutNumber = 0.0f;
+	int stringToNumber = atoi( selectedNumber.c_str( ) );
+	std::string myString = to_string( static_cast<long long>( stringToNumber ) );
+	for( int i = 1; i <= stringToNumber; i*=10 ){
+		parseOutNumber = ( stringToNumber / i ) % 10;
 		listOfSplitNumbers.push_front( parseOutNumber );
 	}
 }
@@ -51,12 +58,12 @@ int main( int argc, char* argv[] ){
 	//	}
 	//	//do something here
 	//}
+	string tempNumber;
 	cout << "Insert a number >> ";
-	cin >> selectedNumber;
+	cin >> tempNumber;
 
 	//senPrimeNumber = atoi( lineBuffer.c_str( ) ) ;
-	SplitNumber( );
-	FindCombinations( );
+	SplitString( tempNumber );
+	//FindCombinations( );
 	return 0;
 }
-
